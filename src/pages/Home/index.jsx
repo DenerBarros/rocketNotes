81019588b117a66fdbfd8a110b227c022e02bafb
DenerBarros <denerbarros.eng@gmail.com>
2022-10-05 14:33:09 -0300

    Criado o filtro e o logotipo

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,37 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
+
+export function Home() {
+    return (
+        <Container>
+            <Brand>
+                <h1>RocketNotes</h1>
+            </Brand>
+
+            <Header />
+
+            <Menu>
+              
+                <li><ButtonText title="Todos" isActive/></li>
+                <li><ButtonText title="React"/></li>
+                <li><ButtonText title="Node"/></li>
+                
+            </Menu>
+
+            <Search>
+
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNote>
+
+            </NewNote>
+
+        </Container>
+    );
+}
