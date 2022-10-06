0be8b28afad2abbd204af8f061ba3fe0a0e5eeee
DenerBarros <denerbarros.eng@gmail.com>
2022-10-06 10:46:49 -0300

    Criado o new a Tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,10 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+
 
 import { Container, Form } from './styles';
 
@@ -18,6 +22,19 @@ export function New() {
 
                     <Input placeholder="Título"/>
                     <Textarea placeholder="Observações" />
+
+                    <Section title="Links úteis" >
+                        <NoteItem  value="https://github.com/DenerBarros" />
+                        <NoteItem  isNew placeholder="Novo Link" />
+                    </Section>    
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem  value="react" />
+                            <NoteItem  isNew placeholder="Nova tag" />
+                        </div>
+                    </Section>
+                <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
