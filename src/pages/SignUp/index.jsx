c9054a3ed7c69f8f70b4d0738a714f4b9266f093
DenerBarros <denerbarros.eng@gmail.com>
2022-10-10 12:02:00 -0300

    Criado o useState da página de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { Container, Form, Background } from './styles';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { api } from '../../services/api'
 
 import { Link } from 'react-router-dom';
 
@@ -7,6 +10,30 @@ import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 export function SignUp() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSignUp() {
+       if(!name || !email ||!password) {
+        return alert("Preencha todos os campos!");
+       }
+    
+    api.post("/users", { name, email, password})
+    .then(() => {
+        alert("Usuário cadastrado com sucessso!");
+    })
+    .catch( error => {
+        if (error.response) {
+            alert(error.response.data.message);
+
+        } else {
+            alert("Não foi possível cadastrar o usuário!");
+        }
+    })
+    }
+
     return (
         <Container>
         <Background />
@@ -19,21 +46,24 @@ export function SignUp() {
                 placeholder="Nome"
                 type="text"
                 icon={FiUser}
+                onChange={e => setName(e.target.value)}
             />
 
             <Input 
                 placeholder="E-mail"
                 type="text"
                 icon={FiMail}
+                onChange={e => setEmail(e.target.value)}
             />
 
             <Input 
                 placeholder="Senha"
                 type="password"
                 icon={FiLock}
+                onChange={e => setPassword(e.target.value)}
             />
 
-            <Button title="Cadastrar" />
+            <Button title="Cadastrar" onClick={handleSignUp} />
             
             
             <Link to="/" >
