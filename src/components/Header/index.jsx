9688c5c42421a6c44903764fb22d478e176c2235
Dener Marcos Tolentino Barros - Unicred Evolução <dener.4022@e-unicred.com.br>
2022-10-18 15:24:37 -0300

    Corrigido a variavel de nome de usuário no componente de header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -25,7 +25,7 @@ export function Header(){
 
                 <div>
                     <span>Bem-vindo</span>
-                    <strong>Dener Barros</strong>
+                    <strong>{user.name}</strong>
                 </div>
             </Profile>
 
